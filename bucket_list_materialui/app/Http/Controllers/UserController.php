865a6997ddb15081583893129ddbe32e7a22e95f
Miwa <miwamurata08@gmail.com>
2022-07-23 10:22:09 -0700

    edit-profile-controller__showEdit--editProfile

--- bucket_list_materialui/app/Http/Controllers/UserController.php
+++ bucket_list_materialui/app/Http/Controllers/UserController.php
@@ -5,75 +5,117 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Profile;
+use Validator;
 
 class UserController extends Controller
 {
-    public function index(User $user){
-        \Log::info('user/index');
-        $user_data=User::with(['profile','likes','bucket_lists'])->select('id','name','email')->find($user->id)->toArray();
-        \Log::debug($user);
-        $is_liked_by_auth=$user->is_liked_by_auth();
-        $user_data['countLikes']=count($user_data['likes']);
-        $user_data['is_liked_by_auth']=$is_liked_by_auth;
-        \Log::debug($user_data);
-       return  view('list_user')->with(['user_data'=>$user_data]);
-    }
-    public function showProfile(User $user){
-        \Log::info('showProfile');
-        $user_data=User::with(['profile','likes'])->select('id','name','email')->find($user->id)->toArray();
-        \Log::debug($user);
-        $is_liked_by_auth=$user->is_liked_by_auth();
-        $user_data['countLikes']=count($user_data['likes']);
-        $user_data['is_liked_by_auth']=$is_liked_by_auth;
-        \Log::debug($user_data);
-      return view('my_profile')->with(['user_data'=>$user_data]);
-    }
+	public function index(User $user){
+		\Log::info('user/index');
+		$user_data=User::with(['profile','likes','bucket_lists'])->select('id','name','email')->find($user->id)->toArray();
+		\Log::debug($user);
+		$is_liked_by_auth=$user->is_liked_by_auth();
+		$user_data['countLikes']=count($user_data['likes']);
+		$user_data['is_liked_by_auth']=$is_liked_by_auth;
+		\Log::debug($user_data);
+	   return  view('list_user')->with(['user_data'=>$user_data]);
+	}
+	public function showProfile(User $user){
+		\Log::info('showProfile');
+		$user_data=User::with(['profile','likes'])->select('id','name','email')->find($user->id)->toArray();
+		\Log::debug($user);
+		$is_liked_by_auth=$user->is_liked_by_auth();
+		$user_data['countLikes']=count($user_data['likes']);
+		$user_data['is_liked_by_auth']=$is_liked_by_auth;
+		\Log::debug($user_data);
+	  return view('my_profile')->with(['user_data'=>$user_data]);
+	}
 
-    public function editProfile(UserRequest $userRequest,User $user){
-       \Log::info('user/editProfile');
-       $user->name=$userRequest->name->save();
-       $profile=Profile::find($user->id);
-       $result=$profile->fill(['photo'=>$userRequest->photo,'question_1'=>$userRequest->question_1,'question_2'=>$userRequest->question_2,'question_3'=>$userRequest->question_3])->save();
-       $result?response()->json($user,$profile):response()->json([],500);
-       return view('my_profile')->with(['edit_mode'=>false]);
-    }
+	public function editProfile(Request $request,User $user){
+	   \Log::info('user/editProfile');
 
-    public function resetPassword(passwordRequest $passwordRequest,User $user){
-        \Log::info('user/resetPassword');
+	   $validator=Validator::make($request->all(),[
+		"photo"=>"string|max:255",
+		"name"=>"string|max:255",
+		"question_1"=>"string |max:500",
+		"question_2"=>"string |max:500",
+		"question_3"=>"string |max:500",
+	   ],[
+		"photo.string"=>"Upload jpg or png file.",
+		"photo.max"=>"Upload the photo within XXX bytes.",
+		"name.string"=>"Data type for name is not valid. ",
+		"name.max"=>"Input name within 255 letters. ",
+		"question_1.string"=>"Data type for answers are not valid. ",
+		"question_2.string"=>"Data type for answers are not valid. ",
+		"question_3.string"=>"Data type for answers are not valid. ",
+		"question_1.max"=>"Input each answer within 500 letters. ",
+		"question_2.max"=>"Input each answer within 500 letters. ",
+		"question_3.max"=>"Input each answer within 500 letters. ",
+	   ]);
+	   if($validator->fails()){
+		return back()
+		->withErrors($validator)
+		->withInput();
 
-        $user->password=\Hash::make($passwordRequest->password);
-        $result=$user->save();
-        $result===true?response()->json($result,201):response()->json([],500);
-    }
+	   }else{
+         $photo=!empty($request->photo)?$request->photho:'./image/no_image.jpg';
+	     $name=!empty($request->name)?$request->name:'No name';
+	     $question_1=!empty($request->question_1)?$request->question_1:'No comment';
+	     $question_2=!empty($request->question_2)?$request->question_2:'No comment';
+	     $question_3=!empty($request->question_3)?$request->question_3:'No comment';
 
-    public function storeFarovite(FavoriteRequest $favoriteRequest){
-        \Log::info('user/storeFavorite');
-        $favoriteRequest->merge([
-            'from_user'=>$favoriteRequest->Auth::id()
-        ]);
-        $favorite=Favorite::create($favoriteRequest->all());
-        $favorite?response()->json($favorite,201):json([],500);
-    }
+         $result=Profile::find($user->id)->fill([
+		  'photo'=>$photo,
+		  'question_1'=>$question_1,
+		  'question_2'=>$question_2,
+		  'question_3'=>$question_3,
+	     ])->save();
 
-    public function deleteFavorite(FavoriteRequest $favoriteRequest,Favorite $favorite){
-        \Log::info('user/deleteFavorite');
-        $result=$favorite->delete();
-        $result===true?response()->json($favorite,201):response()->json([],500);
-    }
+	    if($result===true){
+         return redirect()->route('user.showProfile',['user'=>$user->id]);
 
-    public function storeLike(LikeRequest $likeRequest){
-        \Log::info('user/storeLike');
-        $likeRequest->merge([
-            'from_user'=>Auth::id()
-        ]);
-        $like=Like::create($likeRequest->all());
-        $like?response()->json($favofite,201):json([],500);
-    }
+		}else{
+		  return back()
+		  ->with(['error_edit_profile'=>'Failed to save data. Please try again.']);
+	   }
+	  }
+	}
 
-    public function deleteLike(LikeRequest $likeRequest,Like $like){
-        \Log::info('user/deleteLike');
-        $result=$like->delete();
-        $result===true?response()->json($result,201):response()->json([],500);
-    }
+	public function resetPassword(passwordRequest $passwordRequest,User $user){
+		\Log::info('user/resetPassword');
+
+		$user->password=\Hash::make($passwordRequest->password);
+		$result=$user->save();
+		$result===true?response()->json($result,201):response()->json([],500);
+	}
+
+	public function storeFarovite(FavoriteRequest $favoriteRequest){
+		\Log::info('user/storeFavorite');
+		$favoriteRequest->merge([
+			'from_user'=>$favoriteRequest->Auth::id()
+		]);
+		$favorite=Favorite::create($favoriteRequest->all());
+		$favorite?response()->json($favorite,201):json([],500);
+	}
+
+	public function deleteFavorite(FavoriteRequest $favoriteRequest,Favorite $favorite){
+		\Log::info('user/deleteFavorite');
+		$result=$favorite->delete();
+		$result===true?response()->json($favorite,201):response()->json([],500);
+	}
+
+	public function storeLike(LikeRequest $likeRequest){
+		\Log::info('user/storeLike');
+		$likeRequest->merge([
+			'from_user'=>Auth::id()
+		]);
+		$like=Like::create($likeRequest->all());
+		$like?response()->json($favofite,201):json([],500);
+	}
+
+	public function deleteLike(LikeRequest $likeRequest,Like $like){
+		\Log::info('user/deleteLike');
+		$result=$like->delete();
+		$result===true?response()->json($result,201):response()->json([],500);
+	}
 
 }
--- bucket_list_materialui/resources/views/my_profile.blade.php
+++ bucket_list_materialui/resources/views/my_profile.blade.php
@@ -1,3 +1,7 @@
+@php
+  $name=!empty($user_data['name'])?$user_data['name']:'No name';
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
@@ -5,6 +9,10 @@
   @if(!empty($user_data))
  <div class="row mx-auto">
       <div class="col-lg-4 mx-auto">
+      <form method="post" action={{route('user.editProfile',['user'=>$user_data['id']])}}>
+        @csrf
+        @method('patch')
+
         <div class="mx-auto mb-2 profile-img edit-img">
         @if(!empty($user_data['profile']))
         <img src="{{ asset($user_data['profile']['photo'])}}" class="rounded-circle d-block mx-auto" alt="{{$user_data['name'].'_photo'}}" width="100" height="100" >
@@ -12,23 +20,28 @@
         <img src="xxx" class="img-circle d-block mx-auto" alt="{{$user_data['name'].'_photo'}}"  width="100" height="100">
         @endif
         </div>
-        <h3 class="fw-normal text-center">{{$user_data['name']}}</h3>
-        <div class="text-center">
-          <i class=`bi bi-heart-fill`></i><strong>{{$user_data['countLikes']}}</strong>
+        <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" placeholder="No name" value="{{$name}}">
         </div>
-           <strong class="text-center  d-block">What is your motto?</strong>
-            <p class="text-center">{{!empty($user_data['profile'])?
-              $user_data['profile']['question_1']:
-            'No comment.'}}</p>
-            <strong class="text-center d-block">What is your belief?</strong>
-            <p class="text-center">{{!empty($user_data['profile'])?
-              $user_data['profile']['question_2']:
-            'No comment.'}}</p>
-            <strong class="text-center  d-block">What would you do if you win $100,000?</strong>
-            <p class="text-center">{{!empty($user_data['profile'])?
-              $user_data['profile']['question_3']:
-            'No comment.'}}</p>
-       <button class="btn btn-secondary" type="submit" role="button" aria-disabled="false" href={{route('user.editProfile',['user'=>$user_data['id']])}}>Submit</button>
+        <div class="mb-3">
+           <label for="question_1" class="form-label"><strong class="d-block">What is your motto?</strong></label>
+           <textarea class="form-control" id="question_1" name="question_1" rows="3">{{!empty($user_data['profile'])?$user_data['profile']['question_1']:
+            'No comment.'}}</textarea>
+         </div>
+         <div class="mb-3">
+           <label for="question_2" class="form-label"><strong class="d-block">What is your belief?</strong></label>
+           <textarea class="form-control" id="question_2" name="question_2" rows="3">{{!empty($user_data['profile'])?$user_data['profile']['question_2']:
+            'No comment.'}}</textarea>
+         </div>
+          <div class="mb-3">
+           <label for="question_3" class="form-label"><strong class="d-block">What would you do if you win $100,000?</strong></label>
+           <textarea class="form-control" id="question_3" name="question_3" rows="3">{{!empty($user_data['profile'])?$user_data['profile']['question_3']:
+            'No comment.'}}</textarea>
+         </div>
+
+       <button class="btn btn-secondary" type="submit" role="button" aria-disabled="false" >Submit</button>
+       </form>
        </div>
      @else
     No user data
