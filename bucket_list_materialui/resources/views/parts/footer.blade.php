87b6c4513815c405d6afcc97cbb02558dfd72df4
Miwa <miwamurata08@gmail.com>
2022-08-18 14:29:39 -0700

    footer_fixed

--- bucket_list_materialui/resources/views/parts/footer.blade.php
+++ bucket_list_materialui/resources/views/parts/footer.blade.php
@@ -1,5 +1,5 @@
 
-	<footer class="bd-footer text-center bg-light pt-3 pb-3 footer">
+	<footer class="bd-footer text-center bg-light pt-3 pb-3">
 		<div><a href="{{route('bucket-lists.index')}}" class="text-decoration-none link-dark">My Todo Lists</a></div>
 		<div>Copyright(C) All Rights Reserved.</div>
 	</footer>
