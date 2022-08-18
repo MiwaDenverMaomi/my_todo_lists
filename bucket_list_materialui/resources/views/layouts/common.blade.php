d9e6ae19aa082180b6c4571c42ce1da215e6cbe7
Miwa <miwamurata08@gmail.com>
2022-08-18 14:37:12 -0700

    added_div_on_top_layer

--- bucket_list_materialui/resources/views/layouts/common.blade.php
+++ bucket_list_materialui/resources/views/layouts/common.blade.php
@@ -2,8 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 	@include('parts.head')
 <body>
+	<div style="display: grid;
+  grid-template-rows: auto 1fr auto;
+  grid-template-columns: 100%;
+  min-height: 100vh;">
 	@include('parts.nav')
-	<main class="w-70 p-3 mb-5 main">
+	<main class="w-70 p-3 mb-5">
 		<h1 class="h3 mb-3 fw-normal mb-5 text-center">@yield('title')</h1>
 	 @yield('content')
 	 @include('parts.go_to_top_button')
@@ -13,5 +17,6 @@
 	@endif
 
 	@include('parts.scripts')
+	</div>
 </body>
 </html>
