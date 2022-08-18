eb0435c01b0077dce80c1b46e909ee32fdadd906
Miwa <miwamurata08@gmail.com>
2022-08-18 11:44:37 -0700

    contactcontroller and kernel changed

--- bucket_list_materialui/app/Http/Controllers/ContactController.php
+++ bucket_list_materialui/app/Http/Controllers/ContactController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
-// use Illuminate\Support\Facades\Mail;
-// use App\Mail\Inquiry;
-// use App\Mail\Notice;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Inquiry;
+use App\Mail\Notice;
 use Validator;
 //for sendGrid
-use SendGrid;
-use SendGrid\Mail\Mail;
+// use SendGrid;
+// use SendGrid\Mail\Mail;
 
 class ContactController extends Controller
 {
@@ -30,51 +30,51 @@ class ContactController extends Controller
 	 \Log::info('postContact');
 
 	 $data=$contactRequest->all();
-	 $sendGrid=new SendGrid(getenv('SENDGRID_API_KEY'));
+	//  $sendGrid=new SendGrid(getenv('SENDGRID_API_KEY'));
 
-	 $emailToUser=new Mail();
-	 $emailToUser->setFrom(config('USERNAME'));
-	 $emailToUser->setSubject($contactRequest->title);
-	 $emailToUser->addTo($contactRequest->email);
+	//  $emailToUser=new Mail();
+	//  $emailToUser->setFrom(config('USERNAME'));
+	//  $emailToUser->setSubject($contactRequest->title);
+	//  $emailToUser->addTo($contactRequest->email);
 
-	 $emailToUser->addContent(
-		"text/plain",
-		strval(
-			view(
-				'emails.inquiry',compact('data')
-			)
-		)
-	 );
+	//  $emailToUser->addContent(
+	// 	"text/plain",
+	// 	strval(
+	// 		view(
+	// 			'emails.inquiry',compact('data')
+	// 		)
+	// 	)
+	//  );
 
-	 $emailToAdmin=new Mail();
-	 $emailToAdmin->setFrom(config('MAIL_FROM_ADDRESS'));
-	 $emailToAdmin->setSubject('We have got iquiry!');
-	 $emailToAdmin->addContent(
-		"text/plain",
-		strval(
-			view(
-				'emails.notice',compact('data')
-			)
-		)
-	 );
+	//  $emailToAdmin=new Mail();
+	//  $emailToAdmin->setFrom(config('MAIL_FROM_ADDRESS'));
+	//  $emailToAdmin->setSubject('We have got iquiry!');
+	//  $emailToAdmin->addContent(
+	// 	"text/plain",
+	// 	strval(
+	// 		view(
+	// 			'emails.notice',compact('data')
+	// 		)
+	// 	)
+	//  );
 	 //old codes using without sendGrid
-		// Mail::to($contactRequest->input('email'))->send(new Inquiry($contactRequest));
-		// Mail::to(config('mail.mailers.smtp.username'))->send(new Notice($contactRequest));
+		Mail::to($contactRequest->input('email'))->send(new Inquiry($contactRequest));
+		Mail::to(config('mail.mailers.smtp.username'))->send(new Notice($contactRequest));
 
-		// if(count(Mail::failures())>0){
-		//  //if email is sent
-		//      return view('result',[
-		//      'is_success'=>false,
-		//      'message'=>'Sorry, your email was not sent...'
-		// ]);
+		if(count(Mail::failures())>0){
+		 //if email is sent
+		     return view('result',[
+		     'is_success'=>false,
+		     'message'=>'Sorry, your email was not sent...'
+		]);
 
-		// }else{
-		//     //if email is not sent
-		//     return view('result',[
-		//     'is_success'=>true,
-		//     'message'=>'Email was sent! We will reply in a few days.'
-		//      ]);
-		// }
+		}else{
+		    //if email is not sent
+		    return view('result',[
+		    'is_success'=>true,
+		    'message'=>'Email was sent! We will reply in a few days.'
+		     ]);
+		}
 
 	}
 }
