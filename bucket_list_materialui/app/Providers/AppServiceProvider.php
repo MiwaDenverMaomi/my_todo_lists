0c5459ec2ea2e14c6e4524b89c2f08c1e8b2dae2
Miwa <miwamurata08@gmail.com>
2022-08-19 22:45:57 -0700

    authProvider https back

--- bucket_list_materialui/app/Providers/AppServiceProvider.php
+++ bucket_list_materialui/app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-    //    if (App::environment(['production'])) {
-    //       URL::forceScheme('https');
+       if (App::environment(['production'])) {
+          URL::forceScheme('https');
 
-    //     }
+        }
        $this->app->bind('path.public', function() {
        return base_path().'/public/';
 
