7906b9ca5421e86033a0dc21bd4ed2dcf9b9bb8d
Miwa <miwamurata08@gmail.com>
2022-08-24 13:56:34 -0700

    _ts deleted

--- bucket_list_materialui/resources/_ts/reducers/index.ts
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import { bucketListReducer } from '../reducers/BucketListReducer';
-export default combineReducers({
-  allBucketLists:bucketListReducer
-});
